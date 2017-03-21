b0b3e1eb0361ffdbd8fbb8af60d2eddf73caaaed
Kim, sangki <sang5052003@naver.com>
2017-03-21 14:43:24 +0900

    Question + reply by sangki

---
+++ JobMate/src/jobmate/domain/Reply.java
@@ -0,0 +1,29 @@
+package jobmate.domain;
+
+public class Reply {
+	private int replyNo;
+	private String content;
+	private String customerID;
+	
+	public int getReplyNo() {
+		return replyNo;
+	}
+	public void setReplyNo(int replyNo) {
+		this.replyNo = replyNo;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getCustomerID() {
+		return customerID;
+	}
+	public void setCustomerID(String customerID) {
+		this.customerID = customerID;
+	}
+	
+	
+
+}
